17ab6614f30a63d18f5918d495fd8f01e0cd4808
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-09-29 16:37:25 -0500

    Avance en click.

---
+++ cli/commands/alimentar_distritos.py
@@ -0,0 +1,3 @@
+"""
+Alimentar distritos
+"""
--- cli/commands/alimentar_roles.py
+++ cli/commands/alimentar_roles.py
@@ -1,12 +1,3 @@
 """
 Alimentar roles
 """
-import click
-
-from turnos.blueprints.roles.models import Rol
-
-
-def alimentar_roles():
-    """Alimentar roles"""
-    cantidad = Rol.insert_roles()
-    click.echo(f"  {cantidad} roles alimentados.")
